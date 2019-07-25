c859e56fb2feeffbd495d971bd4f7a9a63f40540
s.hirai <s.hirai@V-TTNS57.srs.local>
2019-07-25 16:44:07 +0900

    組織マスタ一覧の製造

---
+++ src/main/java/jp/co/c_nexco/skf/skf3090/domain/dto/skf3090Sc006common/Skf3090Sc006CommonAsyncDto.java
@@ -0,0 +1,21 @@
+/*
+ * Copyright(c) 2020 NEXCO Systems company limited All rights reserved.
+ */
+package jp.co.c_nexco.skf.skf3090.domain.dto.skf3090Sc006common;
+
+import jp.co.c_nexco.skf.skf3090.domain.dto.common.Skf309040CommonAsyncDto;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 組織マスタ一覧非同期処理共通Dto
+ * 
+ * @author y.morita
+ *
+ */
+@lombok.Data
+@EqualsAndHashCode(callSuper = true)
+public class Skf3090Sc006CommonAsyncDto extends Skf309040CommonAsyncDto {
+
+	private static final long serialVersionUID = -1902278406295003652L;
+
+}
--- src/main/java/jp/co/c_nexco/skf/skf3090/domain/dto/skf3090sc006/Skf3090Sc006SearchDto.java
+++ src/main/java/jp/co/c_nexco/skf/skf3090/domain/dto/skf3090sc006/Skf3090Sc006SearchDto.java
@@ -7,12 +7,14 @@ import jp.co.c_nexco.skf.skf3090.domain.dto.skf3090Sc006common.Skf3090Sc006Commo
 import lombok.EqualsAndHashCode;
 
 /**
- * Skf3090_Sc006InitDto 組織マスタ一覧初期表示Dto
+ * Skf3090Sc006SearchDto 組織マスタ一覧検索ボタン押下Dto
  * 
+ * @author NEXCOシステムズ
+ *
  */
 @lombok.Data
 @EqualsAndHashCode(callSuper = true)
-public class Skf3090Sc006InitDto extends Skf3090Sc006CommonDto {
+public class Skf3090Sc006SearchDto extends Skf3090Sc006CommonDto {
 
 	private static final long serialVersionUID = -1902278406295003652L;
 
