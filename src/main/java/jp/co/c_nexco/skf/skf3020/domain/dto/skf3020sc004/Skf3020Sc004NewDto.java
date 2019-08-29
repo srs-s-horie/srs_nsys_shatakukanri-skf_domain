5289c771b1ac1b2cd192b61c5463c0102b60aa21
t.shiraishi <shiraishi552@oki.com>
2019-08-29 19:18:26 +0900

    転任者一覧追加

---
+++ src/main/java/jp/co/c_nexco/skf/skf3020/domain/dto/skf3020sc004/Skf3020Sc004NewDto.java
@@ -0,0 +1,20 @@
+/*
+ * Copyright(c) 2020 NEXCO Systems company limited All rights reserved.
+ */
+package jp.co.c_nexco.skf.skf3020.domain.dto.skf3020sc004;
+
+import java.util.*;
+import jp.co.c_nexco.skf.skf3020.domain.dto.skf3020Sc004common.Skf3020Sc004CommonDto;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Skf3020_Sc004画面のNewDto。
+ * 
+ */
+@lombok.Data
+@EqualsAndHashCode(callSuper = true)
+public class Skf3020Sc004NewDto extends Skf3020Sc004CommonDto {
+
+	private static final long serialVersionUID = -1902278406295003652L;
+
+}
---
+++ src/main/java/jp/co/c_nexco/skf/skf3020/domain/service/skf3020sc004/Skf3020Sc004Newservice.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright(c) 2020 NEXCO Systems company limited All rights reserved.
+ */
+package jp.co.c_nexco.skf.skf3020.domain.service.skf3020sc004;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import jp.co.c_nexco.nfw.common.utils.NfwStringUtils;
+import jp.co.c_nexco.nfw.webcore.domain.service.BaseServiceAbstract;
+import jp.co.c_nexco.nfw.webcore.domain.service.ServiceHelper;
+import jp.co.c_nexco.skf.common.constants.CodeConstant;
+import jp.co.c_nexco.skf.common.constants.FunctionIdConstant;
+import jp.co.c_nexco.skf.common.constants.MessageIdConstant;
+import jp.co.c_nexco.skf.common.util.SkfOperationLogUtils;
+import jp.co.c_nexco.skf.skf3020.domain.dto.skf3020sc004.Skf3020Sc004NewDto;
+
+/**
+ * Skf3020Sc004RegistService 転任者一覧画面のNewサービス処理クラス。
+ * 
+ * @author NEXCOシステムズ
+ */
+@Service
+public class Skf3020Sc004Newservice extends BaseServiceAbstract<Skf3020Sc004NewDto> {
+
+	@Autowired
+	private SkfOperationLogUtils skfOperationLogUtils;	
+
+	/**
+	 * サービス処理を行う。
+	 * （転任者情報登録画面への遷移のみのため、操作ログのみ）
+	 * 
+	 * @param registDto インプットDTO
+	 * @return 処理結果
+	 * @throws Exception 例外
+	 */
+	@Override
+	public Skf3020Sc004NewDto index(Skf3020Sc004NewDto newDto) throws Exception {
+		
+		// 操作ログを出力する
+		skfOperationLogUtils.setAccessLog("新規", CodeConstant.C001, newDto.getPageId());		
+		
+		return newDto;
+	}
+	
+
+}
