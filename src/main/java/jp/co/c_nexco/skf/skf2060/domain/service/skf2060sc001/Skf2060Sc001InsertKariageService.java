6c7b6296e04961fa189979b6489ef95f1d560b90
s.horie <s.horie@srsys.jp>
2019-09-05 09:17:14 +0900

    借上候補物件登録画面

--- src/main/java/jp/co/c_nexco/skf/skf2060/domain/service/skf2060sc001/Skf2060Sc001InsertKariageService.java
+++ src/main/java/jp/co/c_nexco/skf/skf2060/domain/service/skf2060sc001/Skf2060Sc001InsertKariageService.java
@@ -3,147 +3,72 @@
  */
 package jp.co.c_nexco.skf.skf2060.domain.service.skf2060sc001;
 
-import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.regex.Pattern;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf2010Sc004.Skf2010Sc004InsertBihinHenkyakuInfoExp;
-import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf2060Sc001.Skf2060Sc001GetKariageBukkenExp;
-import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf2060Sc001.Skf2060Sc001GetKariageBukkenExpParameter;
-import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf2060Sc001.Skf2060Sc001PostalCodeAddressExp;
-import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf2060Sc001.Skf2060Sc001PostalCodeAddressExpParameter;
-import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf3090Sc004.Skf3090Sc004GetListTableDataExp;
-import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf3090Sc004.Skf3090Sc004GetListTableDataExpParameter;
-import jp.co.c_nexco.businesscommon.entity.skf.exp.SkfGetInfoUtils.SkfGetInfoUtilsGetShainInfoExp;
-import jp.co.c_nexco.businesscommon.repository.skf.exp.Skf2060Sc001.Skf2060Sc001GetKariageBukkenExpRepository;
-import jp.co.c_nexco.businesscommon.repository.skf.exp.Skf2060Sc001.Skf2060Sc001PostalCodeAddressExpRepository;
-import jp.co.c_nexco.businesscommon.repository.skf.exp.Skf3090Sc004.Skf3090Sc004GetListTableDataExpRepository;
-import jp.co.c_nexco.nfw.common.utils.CheckUtils;
 import jp.co.c_nexco.nfw.webcore.domain.service.BaseServiceAbstract;
 import jp.co.c_nexco.nfw.webcore.domain.service.ServiceHelper;
 import jp.co.c_nexco.skf.common.constants.CodeConstant;
 import jp.co.c_nexco.skf.common.constants.MessageIdConstant;
-import jp.co.c_nexco.skf.skf2010.domain.dto.skf2010sc008.Skf2010Sc008SelectDto;
-import jp.co.c_nexco.skf.skf2010.domain.service.skf2010sc008.Skf2010Sc008SharedService;
+import jp.co.c_nexco.skf.common.util.SkfOperationLogUtils;
 import jp.co.c_nexco.skf.skf2060.domain.dto.skf2060sc001.Skf2060Sc001InsertKariageDto;
 
 /**
- * TestPrjTop画面のSearchAddressサービス処理クラス。　 
+ * TestPrjTop画面のInsertKariageサービス処理クラス。　 
  * 
  */
 @Service
 public class Skf2060Sc001InsertKariageService extends BaseServiceAbstract<Skf2060Sc001InsertKariageDto> {
 	
 	@Autowired
-	private Skf2060Sc001GetKariageBukkenExpRepository skf2060Sc001GetKariageBukkenExpRepository;
-	@Autowired
 	private Skf2060Sc001SharedService skf2060Sc001SharedService;
+	@Autowired
+	private SkfOperationLogUtils skfOperationLogUtils;
 	
 	private String companyCd = CodeConstant.C001;
 	
 	/**
 	 * サービス処理を行う。　
 	 * 
-	 * @param initDto
-	 *            インプットDTO
+	 * @param insertDto DTO
 	 * @return 処理結果
-	 * @throws Exception
-	 *             例外
+	 * @throws Exception 例外
 	 */
 	@Override
 	public Skf2060Sc001InsertKariageDto index(Skf2060Sc001InsertKariageDto insertDto) throws Exception {
 		
 		insertDto.setPageTitleKey(MessageIdConstant.SKF2060_SC001_TITLE);
 		
+		// 操作ログを出力
+		skfOperationLogUtils.setAccessLog("追加", companyCd, insertDto.getPageId());
+		
 		boolean insertCheck = false;
 		
-		if(isValidateInput(insertDto)){
-			if(isValidateRepeat(insertDto)){
-				insertCheck = skf2060Sc001SharedService.insertKariageKohoInfo(companyCd, insertDto.getShatakuName(), insertDto.getAddress());
-			}
-		}
+		//借上候補物件テーブルへ登録
+		insertCheck = skf2060Sc001SharedService.insertKariageKohoInfo(companyCd, insertDto.getShatakuName(), insertDto.getAddress());
 		
-		if(!(insertCheck)){
-			System.out.println("insertできてないだとぉ");
-		}else{
-			System.out.println("よっしゃあああああああああああ");
+		//登録判定
+		if(insertCheck){
+			//登録できた場合
+			//入力欄の「借上社宅名」と「社宅所在地」を空白にする
 			insertDto.setShatakuName("");
+			insertDto.setPostalCd("");
 			insertDto.setAddress("");
+		}else{
+			//登録できなかった場合、エラーメッセージ
+			ServiceHelper.addErrorResultMessage(insertDto, null, MessageIdConstant.E_SKF_1073);
 		}
 		
 		// リストデータ取得用
 		List<Map<String, Object>> dataParamList = new ArrayList<Map<String, Object>>();
 		boolean itiranFlg = true;
-		dataParamList = skf2060Sc001SharedService.getDataParamList(itiranFlg);
+		dataParamList = skf2060Sc001SharedService.getDataParamList(itiranFlg, insertDto.getShainNo(), insertDto.getApplNo());
 		insertDto.setListTableData(dataParamList);
 		
 		return insertDto;
 	}
 	
-	/**
-	 * 入力チェックを行う。.
-	 * 
-	 * @param insertDto *indexメソッドの引数のDTO。入力チェックエラーの場合、エラーメッセージを詰める。
-	 * @return 入力チェック結果。trueの場合チェックOK。
-	 */
-	public boolean isValidateInput(Skf2060Sc001InsertKariageDto insertDto){
-		boolean inputCheck = true;
-		
-		insertDto.setShatakuNameError("");
-		insertDto.setAddressError("");
-		
-		//借上社宅名の未入力チェック
-		if(insertDto.getShatakuName() == null || CheckUtils.isEmpty(insertDto.getShatakuName().trim())){
-			ServiceHelper.addErrorResultMessage(insertDto, null, MessageIdConstant.E_SKF_1048, "借上社宅名");
-			insertDto.setShatakuNameError(CodeConstant.NFW_VALIDATION_ERROR);
-			inputCheck = false;
-		}
-		//住所の未入力チェック
-		if(insertDto.getAddress() == null || CheckUtils.isEmpty(insertDto.getAddress().trim())){
-			ServiceHelper.addErrorResultMessage(insertDto, null, MessageIdConstant.E_SKF_1048, "住所");
-			insertDto.setAddressError(CodeConstant.NFW_VALIDATION_ERROR);
-			inputCheck = false;
-		}
-	    return inputCheck;
-	}
-	
-	/**
-	 * 重複チェックを行う。.
-	 * 
-	 * @param insertDto *indexメソッドの引数のDTO。入力チェックエラーの場合、エラーメッセージを詰める?
-	 * @return 入力チェック結果。trueの場合チェックOK。
-	 */
-	public boolean isValidateRepeat(Skf2060Sc001InsertKariageDto insertDto){
-		boolean repeatCheck = true;
-		
-		List<Skf2060Sc001GetKariageBukkenExp> resultListTableData = new ArrayList<Skf2060Sc001GetKariageBukkenExp>();
-		Skf2060Sc001GetKariageBukkenExpParameter param = new Skf2060Sc001GetKariageBukkenExpParameter();
-		param.setCompanyCd(companyCd);
-		param.setShatakuName(insertDto.getShatakuName());
-		param.setAddress(insertDto.getAddress());
-		param.setItiranFlg(false);
-		
-		resultListTableData = skf2060Sc001GetKariageBukkenExpRepository.getKariageBukken(param);
-		//借上候補物件テーブルに重複するデータが存在する場合
-		if(resultListTableData != null){
-			//多分１件しか出ない
-			for(int i = 0; i < resultListTableData.size(); i++){
-				//提示可能or提示済の物件であれば重複エラーメッセージを表示する
-				if(resultListTableData.get(i).getTeijiFlg().equals("0")||resultListTableData.get(i).getTeijiFlg().equals("1")){
-					ServiceHelper.addErrorResultMessage(insertDto, null, MessageIdConstant.E_SKF_1033,"借上社宅名");
-					repeatCheck = false;
-				//選択済の物件であれば確認メッセージを表示したいなぁ
-				}else if(resultListTableData.get(i).getTeijiFlg().equals("2")){
-				//TODO 確認ダイアログを表示するようななにかをしたい
-					System.out.println("選択済物件(´・ω・｀)");
-				}
-			}
-		}	
-		return repeatCheck;
-	}
 }
--- src/main/java/jp/co/c_nexco/skf/skf2060/domain/service/skf2060sc001/Skf2060Sc001SupportService.java
+++ src/main/java/jp/co/c_nexco/skf/skf2060/domain/service/skf2060sc001/Skf2060Sc001SupportService.java
@@ -5,7 +5,6 @@ package jp.co.c_nexco.skf.skf2060.domain.service.skf2060sc001;
 
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -13,18 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf2060Sc001.Skf2060Sc001GetApplHistoryExp;
-import jp.co.c_nexco.nfw.common.bean.MenuScopeSessionBean;
 import jp.co.c_nexco.nfw.webcore.domain.service.BaseServiceAbstract;
 import jp.co.c_nexco.skf.common.constants.CodeConstant;
 import jp.co.c_nexco.skf.common.constants.MessageIdConstant;
 import jp.co.c_nexco.skf.common.constants.SkfCommonConstant;
 import jp.co.c_nexco.skf.common.util.SkfDateFormatUtils;
-import jp.co.c_nexco.skf.skf2060.domain.dto.skf2060sc001.Skf2060Sc001InitDto;
 import jp.co.c_nexco.skf.skf2060.domain.dto.skf2060sc001.Skf2060Sc001SupportDto;
-import jp.co.c_nexco.skf.skf3090.domain.service.skf3090sc004.Skf3090Sc004SharedService;
 
 /**
- * TestPrjTop画面のInitサービス処理クラス。　 
+ * TestPrjTop画面のSupportサービス処理クラス。　 
  * 
  */
 @Service
@@ -34,8 +30,6 @@ public class Skf2060Sc001SupportService extends BaseServiceAbstract<Skf2060Sc001
 	private Skf2060Sc001SharedService skf2060Sc001SharedService;
 	@Autowired
 	private SkfDateFormatUtils skfDateFormatUtils;
-	@Autowired
-	private MenuScopeSessionBean sessionBean;
 	
 	private String companyCd = CodeConstant.C001;
 	
@@ -43,11 +37,9 @@ public class Skf2060Sc001SupportService extends BaseServiceAbstract<Skf2060Sc001
 	/**
 	 * サービス処理を行う。　
 	 * 
-	 * @param initDto
-	 *            インプットDTO
+	 * @param supportDto DTO
 	 * @return 処理結果
-	 * @throws Exception
-	 *             例外
+	 * @throws Exception 例外
 	 */
 	@Override
 	public Skf2060Sc001SupportDto index(Skf2060Sc001SupportDto supportDto) throws Exception {
@@ -57,7 +49,7 @@ public class Skf2060Sc001SupportService extends BaseServiceAbstract<Skf2060Sc001
 		// リストデータ取得用
 		Skf2060Sc001GetApplHistoryExp resultData = new Skf2060Sc001GetApplHistoryExp();
 		//会社コードと社員番号からデータ取得
-		resultData = skf2060Sc001SharedService.getApplHistoryInfo(companyCd, supportDto.getPresentedNo(), null);
+		resultData = skf2060Sc001SharedService.getApplHistoryInfo(companyCd, supportDto.getShainNo(), null);
 		//取得できた場合
 		if(resultData != null){
 			//DateからStringへ
@@ -65,22 +57,18 @@ public class Skf2060Sc001SupportService extends BaseServiceAbstract<Skf2060Sc001
 			//取得した値をDtoに設定
 			supportDto.setPresentedStatus(resultData.getApplStatus());
 			supportDto.setPresentedDate(presentedDate);	
-			// TODO セッションに申請書類情報を設定(セッション名どうしましょ）
-			sessionBean.put("getApplHistoryResultData", resultData);
-			// TODO 隠し要素に更新日時を設定
+			// TODO 隠し要素に設定？
+			supportDto.setApplNo(resultData.getApplNo());
+			// 隠し要素に更新日時を設定
 			Date updateDate = new Date();
 			String updateDateFormat = skfDateFormatUtils.dateFormatFromDate(updateDate, SkfCommonConstant.YMD_STYLE_YYYYMMDD_SLASH);
 			supportDto.setUpdateDate(updateDateFormat);
-			
-		//取得できなかった場合
-		}else{
-			
 		}
 		
 		// リストデータ取得用
 		List<Map<String, Object>> dataParamList = new ArrayList<Map<String, Object>>();
 		boolean itiranFlg = true;
-		dataParamList = skf2060Sc001SharedService.getDataParamList(itiranFlg);
+		dataParamList = skf2060Sc001SharedService.getDataParamList(itiranFlg, supportDto.getShainNo(), supportDto.getApplNo());
 		supportDto.setListTableData(dataParamList);
 		return supportDto;
 	}
