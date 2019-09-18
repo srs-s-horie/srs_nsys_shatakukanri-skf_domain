65b81d1117e2f4d04170746e9b38213bbf8cf26c
t.masuda <t.masuda@srsys.jp>
2019-09-18 15:32:52 +0900

    備品希望申請画面とカレンダー処理修正

---
+++ src/main/java/jp/co/c_nexco/skf/skf2030/domain/dto/skf2030sc002/Skf2030Sc002RevisionDto.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright(c) 2020 NEXCO Systems company limited All rights reserved.
+ */
+package jp.co.c_nexco.skf.skf2030.domain.dto.skf2030sc002;
+
+import java.util.*;
+import jp.co.c_nexco.skf.skf2030.domain.dto.skf2030Sc002common.Skf2030Sc002CommonDto;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Skf2030_Sc002画面の修正依頼処理Dto。
+ * 
+ */
+@lombok.Data
+@EqualsAndHashCode(callSuper = true)
+public class Skf2030Sc002RevisionDto extends Skf2030Sc002CommonDto {
+
+	private static final long serialVersionUID = -1902278406295003652L;
+
+	private boolean status24Flag;
+
+}
--- src/main/java/jp/co/c_nexco/skf/skf2030/domain/service/skf2030sc002/Skf2030Sc002ApplyService.java
+++ src/main/java/jp/co/c_nexco/skf/skf2030/domain/service/skf2030sc002/Skf2030Sc002ApplyService.java
@@ -6,25 +6,28 @@ package jp.co.c_nexco.skf.skf2030.domain.service.skf2030sc002;
 import java.util.HashMap;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+import jp.co.c_nexco.nfw.common.utils.NfwStringUtils;
+import jp.co.c_nexco.nfw.common.utils.PropertyUtils;
+import jp.co.c_nexco.nfw.webcore.app.TransferPageInfo;
 import jp.co.c_nexco.nfw.webcore.domain.model.BaseDto;
 import jp.co.c_nexco.nfw.webcore.domain.service.BaseServiceAbstract;
+import jp.co.c_nexco.nfw.webcore.domain.service.ServiceHelper;
 import jp.co.c_nexco.skf.common.constants.CodeConstant;
 import jp.co.c_nexco.skf.common.constants.FunctionIdConstant;
 import jp.co.c_nexco.skf.common.constants.MessageIdConstant;
 import jp.co.c_nexco.skf.common.util.SkfLoginUserInfoUtils;
 import jp.co.c_nexco.skf.common.util.SkfOperationGuideUtils;
 import jp.co.c_nexco.skf.common.util.SkfOperationLogUtils;
-import jp.co.c_nexco.skf.skf2030.domain.dto.skf2030sc002.Skf2030Sc002InitDto;
+import jp.co.c_nexco.skf.skf2030.domain.dto.skf2030sc002.Skf2030Sc002ApplyDto;
 
 /**
- * Skf2030Sc002 備品希望申請（アウトソース用)初期表示処理クラス
+ * Skf2030Sc002 備品希望申請（アウトソース用)申請処理クラス
  *
  * @author NEXCOシステムズ
  */
 @Service
-public class Skf2030Sc002InitService extends BaseServiceAbstract<Skf2030Sc002InitDto> {
+public class Skf2030Sc002ApplyService extends BaseServiceAbstract<Skf2030Sc002ApplyDto> {
 
 	@Autowired
 	private Skf2030Sc002SharedService skf2030Sc002SharedService;
@@ -33,22 +36,20 @@ public class Skf2030Sc002InitService extends BaseServiceAbstract<Skf2030Sc002Ini
 	private SkfLoginUserInfoUtils skfLoginUserInfoUtils;
 	@Autowired
 	private SkfOperationLogUtils skfOperationLogUtils;
-	@Autowired
-	private SkfOperationGuideUtils skfOperationGuideUtils;
 
 	/**
 	 * サービス処理を行う。
 	 * 
-	 * @param initDto インプットDTO
+	 * @param applyDto インプットDTO
 	 * @return 処理結果
 	 * @throws Exception 例外
 	 */
 	@Override
-	public BaseDto index(Skf2030Sc002InitDto initDto) throws Exception {
+	public BaseDto index(Skf2030Sc002ApplyDto applyDto) throws Exception {
 		// 操作ログ出力
-		skfOperationLogUtils.setAccessLog("初期表示処理開始", CodeConstant.C001, FunctionIdConstant.SKF2030_SC002);
+		skfOperationLogUtils.setAccessLog("申請処理開始", CodeConstant.C001, FunctionIdConstant.SKF2030_SC002);
 		// タイトル設定
-		initDto.setPageTitleKey(MessageIdConstant.SKF2030_SC002_TITLE);
+		applyDto.setPageTitleKey(MessageIdConstant.SKF2030_SC002_TITLE);
 
 		// ログインユーザー情報取得
 		Map<String, String> loginUserInfo = skfLoginUserInfoUtils
@@ -56,23 +57,23 @@ public class Skf2030Sc002InitService extends BaseServiceAbstract<Skf2030Sc002Ini
 
 		// 申請情報設定
 		Map<String, String> applInfo = new HashMap<String, String>();
-		applInfo.put("status", initDto.getSendApplStatus());
-		applInfo.put("applNo", initDto.getApplNo());
-		applInfo.put("applId", initDto.getApplId());
+		applInfo.put("status", applyDto.getSendApplStatus());
+		applInfo.put("applNo", applyDto.getApplNo());
+		applInfo.put("applId", applyDto.getApplId());
 
-		// 初期表示
-		boolean result = skf2030Sc002SharedService.setDisplayData(initDto, loginUserInfo, applInfo);
-		if (!result) {
-			throwBusinessExceptionIfErrors(initDto.getResultMessages());
-			initDto.setMaskPattern("ERR");
+		String execName = "apply";
+
+		boolean updResult = skf2030Sc002SharedService.updateDispInfo(execName, applyDto, applInfo, loginUserInfo);
+		if (!updResult) {
+			throwBusinessExceptionIfErrors(applyDto.getResultMessages());
 		}
 
-		skf2030Sc002SharedService.setEnabled(initDto, applInfo);
+		// 前の画面に遷移する
+		TransferPageInfo tpi = TransferPageInfo.nextPage(FunctionIdConstant.SKF2010_SC005);
+		tpi.addResultMessage(MessageIdConstant.I_SKF_2030);
+		applyDto.setTransferPageInfo(tpi);
 
-		// 操作ガイド取得
-		initDto.setOperationGuide(skfOperationGuideUtils.getOperationGuide(FunctionIdConstant.SKF2030_SC002));
-
-		return initDto;
+		return applyDto;
 	}
 
 }
