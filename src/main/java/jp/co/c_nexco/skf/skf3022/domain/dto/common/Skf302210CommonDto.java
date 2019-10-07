a0a08292ba6188e0cd89ec45bb35c0101fc4d855
ya.kawasaki <kawasaki837@oki.com>
2019-10-07 17:51:37 +0900

    駐車場契約画面修正

--- src/main/java/jp/co/c_nexco/skf/skf3022/domain/dto/common/Skf302210CommonDto.java
+++ src/main/java/jp/co/c_nexco/skf/skf3022/domain/dto/common/Skf302210CommonDto.java
@@ -1,15 +1,12 @@
 package jp.co.c_nexco.skf.skf3022.domain.dto.common;
 
-import java.util.List;
-import java.util.Map;
-
-import jp.co.c_nexco.nfw.webcore.app.DownloadFile;
 import jp.co.c_nexco.nfw.webcore.domain.model.BaseDto;
 import lombok.EqualsAndHashCode;
 
 /**
- * TestPrjTop画面のInitDto。
- * 
+ * Skf302210CommonDto 提示データ共通DTO
+ *
+ * @author NEXCOシステムズ
  */
 @lombok.Data
 @EqualsAndHashCode(callSuper = true)
@@ -19,4 +16,39 @@ public class Skf302210CommonDto extends BaseDto {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	//提示データ一覧画面連携項目
+	//提示番号
+	private String hdnTeijiNo;
+	//入居予定日
+	private String hdnNyukyoDate;
+	//退居予定日
+	private String hdnTaikyoDate;
+	//申請書類管理番号
+	private String hdnShoruikanriNo;
+	//入退居区分
+	private String hdnNyutaikyoKbn;
+	//申請区分
+	private String hdnApplKbn;
+	//社員番号変更フラグ
+	private String hdnShainNoChangeFlg;
+
+	//提示データ一覧検索条件退避
+	//社員番号
+	private String searchInfoShainNo;
+	//社員氏名
+	private String searchInfoShainName;
+	//社宅名
+	private String searchInfoShatakuName;
+	//入退居区分
+	private String searchInfoNyutaikyoKbn;
+	//社宅提示状況
+	private String searchInfoStJyokyo;
+	//社宅提示確認督促
+	private String searchInfoStKakunin;
+	//備品提示状況
+	private String searchInfoBhJyokyo;
+	//備品提示確認督促
+	private String searchInfoBhKakunin;
+	//備品搬入搬出督促
+	private String searchInfoMoveInout;
 }
