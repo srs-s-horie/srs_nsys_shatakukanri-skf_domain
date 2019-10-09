fe1914f0c5877e8e9f4752cd1a2647af5989f8dc
t.masuda <t.masuda@srsys.jp>
2019-10-09 11:15:45 +0900

    不要なソースを削除

--- src/main/java/jp/co/c_nexco/skf/skf2010/domain/service/skf2010sc003/Skf2010Sc003TransferService.java
+++ src/main/java/jp/co/c_nexco/skf/skf2010/domain/service/skf2010sc003/Skf2010Sc003TransferService.java
@@ -3,15 +3,9 @@
  */
 package jp.co.c_nexco.skf.skf2010.domain.service.skf2010sc003;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import edu.emory.mathcs.backport.java.util.Arrays;
-import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf2010Sc003.Skf2010Sc003GetApplHistoryStatusInfoExp;
 import jp.co.c_nexco.nfw.common.utils.CheckUtils;
 import jp.co.c_nexco.nfw.common.utils.CopyUtils;
 import jp.co.c_nexco.nfw.webcore.app.BaseForm;
@@ -19,13 +13,9 @@ import jp.co.c_nexco.nfw.webcore.app.FormHelper;
 import jp.co.c_nexco.nfw.webcore.app.TransferPageInfo;
 import jp.co.c_nexco.nfw.webcore.domain.model.BaseDto;
 import jp.co.c_nexco.nfw.webcore.domain.service.BaseServiceAbstract;
-import jp.co.c_nexco.nfw.webcore.domain.service.ServiceHelper;
 import jp.co.c_nexco.skf.common.constants.CodeConstant;
 import jp.co.c_nexco.skf.common.constants.FunctionIdConstant;
-import jp.co.c_nexco.skf.common.constants.MessageIdConstant;
 import jp.co.c_nexco.skf.common.constants.SkfCommonConstant;
-import jp.co.c_nexco.skf.common.util.SkfDateFormatUtils;
-import jp.co.c_nexco.skf.common.util.SkfLoginUserInfoUtils;
 import jp.co.c_nexco.skf.skf2010.domain.dto.skf2010sc003.Skf2010Sc003TransferDto;
 
 /**
@@ -35,20 +25,6 @@ import jp.co.c_nexco.skf.skf2010.domain.dto.skf2010sc003.Skf2010Sc003TransferDto
  */
 @Service
 public class Skf2010Sc003TransferService extends BaseServiceAbstract<Skf2010Sc003TransferDto> {
-
-	@Autowired
-	private Skf2010Sc003SharedService skf2010Sc003SharedService;
-	@Autowired
-	private SkfDateFormatUtils skfDateFormatUtils;
-	@Autowired
-	private SkfLoginUserInfoUtils skfLoginUserInfoUtils;
-
-	@Value("${skf2010.skf2010_sc005.search_max_count}")
-	private String searchMaxCount;
-	@Value("${skf.common.validate_error}")
-	private String validationErrorCode;
-
-	private String pattern = "yyyyMMdd";
 
 	/**
 	 * サービス処理を行う。
@@ -106,33 +82,6 @@ public class Skf2010Sc003TransferService extends BaseServiceAbstract<Skf2010Sc00
 		transDto.setTransferPageInfo(tpi);
 
 		return transDto;
-	}
-
-	@SuppressWarnings("unchecked")
-	private List<Skf2010Sc003GetApplHistoryStatusInfoExp> getApplHistoryList(Skf2010Sc003TransferDto dto) {
-		Map<String, String> loginUserInfo = skfLoginUserInfoUtils
-				.getSkfLoginUserInfoFromAlterLogin(menuScopeSessionBean);
-		String shainNo = loginUserInfo.get("shainNo");
-
-		String applDateFrom = skfDateFormatUtils.dateFormatFromString(dto.getApplDateFrom(), pattern);
-		String applDateTo = skfDateFormatUtils.dateFormatFromString(dto.getApplDateTo(), pattern);
-
-		String agreDateFrom = skfDateFormatUtils.dateFormatFromString(dto.getAgreDateFrom(), pattern);
-		String agreDateTo = skfDateFormatUtils.dateFormatFromString(dto.getAgreDateTo(), pattern);
-
-		String applName = dto.getApplName();
-
-		String[] applStatus = dto.getApplStatus();
-		List<String> applStatusList = new ArrayList<String>();
-		if (applStatus != null && applStatus.length > 0) {
-			applStatusList = Arrays.asList(applStatus);
-		}
-
-		// 検索結果取得
-		List<Skf2010Sc003GetApplHistoryStatusInfoExp> resultList = skf2010Sc003SharedService.getApplHistoryStatusInfo(
-				shainNo, applDateFrom, applDateTo, agreDateFrom, agreDateTo, applName, applStatusList);
-
-		return resultList;
 	}
 
 }
