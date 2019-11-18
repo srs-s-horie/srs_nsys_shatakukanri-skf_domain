3a55a954a75b6a61a47a8a638a5758ca967197af
t.funato <t.funato@srsys.jp>
2019-11-18 18:03:02 +0900

    退居届障害修正

--- src/main/java/jp/co/c_nexco/skf/skf2040/domain/dto/skf2040sc001/Skf2040Sc001ChangeDropDownAsyncDto.java
+++ src/main/java/jp/co/c_nexco/skf/skf2040/domain/dto/skf2040sc001/Skf2040Sc001ChangeDropDownAsyncDto.java
@@ -5,7 +5,6 @@ package jp.co.c_nexco.skf.skf2040.domain.dto.skf2040sc001;
 
 import java.util.List;
 import java.util.Map;
-
 import jp.co.c_nexco.businesscommon.entity.skf.exp.Skf2020Sc002.Skf2020Sc002GetShatakuInfoExp;
 import jp.co.c_nexco.skf.skf2040.domain.dto.skf2040Sc001common.Skf2040Sc001CommonAsyncDto;
 import lombok.EqualsAndHashCode;
@@ -19,76 +18,78 @@ import lombok.EqualsAndHashCode;
 @EqualsAndHashCode(callSuper = true)
 public class Skf2040Sc001ChangeDropDownAsyncDto extends Skf2040Sc001CommonAsyncDto {
 
-    /**
-     * シリアルナンバー
-     */
-    private static final long serialVersionUID = 1L;
+	/**
+	 * シリアルナンバー
+	 */
+	private static final long serialVersionUID = 1L;
 
-    // 駐車場2台フラグ
-    private boolean parkingFullFlg;
+	// 駐車場2台フラグ
+	private boolean parkingFullFlg;
 
-    /**
-     * ドロップダウン
-     */
-    /** ドロップダウン：現居住社宅名リスト */
-    private List<Map<String, Object>> ddlNowShatakuNameList;
-    private List<Skf2020Sc002GetShatakuInfoExp> shatakuList;
+	/**
+	 * ドロップダウン
+	 */
+	/** ドロップダウン：現居住社宅名リスト */
+	private List<Map<String, Object>> ddlNowShatakuNameList;
+	private List<Skf2020Sc002GetShatakuInfoExp> shatakuList;
 
-    /**
-     * 申請者
-     */
-    // 社員番号
-    private String shainNo;
+	/**
+	 * 申請者
+	 */
+	// 社員番号
+	private String shainNo;
 
-       /**
-     * 現居住社宅
-     */
-    // 現入居社宅
-    private String nowShataku;
-    // 保有社宅名
-    private String nowShatakuName;
-    // 室番号
-    private String nowShatakuNo;
-    // 規格（間取り）
-    private String nowShatakuKikaku;
-    // 規格名称（間取り）
-    private String nowShatakuKikakuName;
-    // 面積
-    private String nowShatakuMenseki;
-    // 社宅管理ID
-    private long shatakuKanriId;
-    // 現住所
-    private String nowAddress;
-    // 駐車場1台目
-    private String parking1stPlace;
-    // 駐車場2台目
-    private String parking2ndPlace;
+	/**
+	 * 現居住社宅
+	 */
+	// 現入居社宅
+	private String nowShataku;
+	// 保有社宅名
+	private String nowShatakuName;
+	// 室番号
+	private String nowShatakuNo;
+	// 規格（間取り）
+	private String nowShatakuKikaku;
+	// 規格名称（間取り）
+	private String nowShatakuKikakuName;
+	// 面積
+	private String nowShatakuMenseki;
+	// 社宅管理ID
+	private long shatakuKanriId;
+	// 現住所
+	private String nowAddress;
+	// 駐車場1台目
+	private String parking1stPlace;
+	// 駐車場2台目
+	private String parking2ndPlace;
 
-    // 返却備品HTML
-    private String returnEquipment;
-    
-    /**
-     * Hidden
-     */
-    // 社宅管理ID
-    private long hdnShatakuKanriId;
-    // 選択された社宅名
-    private String hdnSelectedNowShatakuName;
+	// 返却備品HTML
+	private String returnEquipment;
 
-    // 規格(間取り)
-    private String hdnShatakuKikakuKbn;
+	/**
+	 * Hidden
+	 */
+	// 社宅管理ID
+	private long hdnShatakuKanriId;
+	// 選択された社宅名
+	private String hdnSelectedNowShatakuName;
 
-    // 現在の位置番号
-    private String hdnParking1stNumber;
+	// 規格(間取り)
+	private String hdnShatakuKikakuKbn;
 
-    // 現在の位置番号2
-    private String hdnParking2ndNumber;
+	// 現在の位置番号
+	private String hdnParking1stNumber;
 
-    // 現居住社宅管理番号
-    private long hdnNowShatakuKanriNo;
-    // 現居住社宅部屋管理番号
-    private long hdnNowShatakuRoomKanriNo;
-    // 備品返却有無
-    private String hdnBihinHenkyakuUmu;
+	// 現在の位置番号2
+	private String hdnParking2ndNumber;
+
+	// 現居住社宅管理番号
+	private long hdnNowShatakuKanriNo;
+	// 現居住社宅部屋番号
+	private String hdnNowShatakuRoomNo;
+	// 現居住社宅部屋管理番号
+	private long hdnNowShatakuRoomKanriNo;
+	// 備品返却有無
+	private String hdnBihinHenkyakuUmu;
 
 }
