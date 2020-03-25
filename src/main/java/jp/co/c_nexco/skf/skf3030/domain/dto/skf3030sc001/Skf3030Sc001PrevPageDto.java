a75f957bfb2635eb264a73b36c4e7a9d1a78f148
n.kamiushiya <kamiushiya877@oki.com>
2020-03-25 16:49:03 +0900

    社宅管理台帳 「戻る」対応

---
+++ src/main/java/jp/co/c_nexco/skf/skf3030/domain/dto/skf3030sc001/Skf3030Sc001PrevPageDto.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright(c) 2020 NEXCO Systems company limited All rights reserved.
+ */
+package jp.co.c_nexco.skf.skf3030.domain.dto.skf3030sc001;
+
+import jp.co.c_nexco.skf.skf3030.domain.dto.skf3030Sc001common.Skf3030Sc001CommonDto;
+import lombok.EqualsAndHashCode;
+
+/**
+ * Skf3030Sc001PrevPageDtoのDto。
+ * 
+ * @author NEXCOシステムズ
+ */
+@lombok.Data
+@EqualsAndHashCode(callSuper = true)
+public class Skf3030Sc001PrevPageDto extends Skf3030Sc001CommonDto {
+	
+	private static final long serialVersionUID = -1902278406295003652L;
+}
